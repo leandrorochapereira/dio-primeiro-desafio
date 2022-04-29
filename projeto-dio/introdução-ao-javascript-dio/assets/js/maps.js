2bf37e800a3e9aa1abf63af3c7a0f209efb7087b
Leo <lrp.0689@gmail.com>
2022-04-29 10:43:22 -0300

    Adicionando coleções Map e Set

---
+++ projeto-dio/introdução-ao-javascript-dio/assets/js/maps.js
@@ -0,0 +1,29 @@
+/*Criando uma função que retorna o nome dos membros de um Map que tem o papel "ADMIN" no sistema
+1 - Crie uma função getAdmins que recebe um Map;
+2 - Crie um Map e popule-o com nomes de usuários e seus papeis no sistema. (Ex: 'Luiz' -> 'Admin');
+3 - Dentro de getAdmins, utilize o loop for...of para retornar uma lista com os nomes dos usuários que são administradores.
+*/
+
+
+function getAdmins(map) {
+
+    let admins = [];
+
+    for([key, value] of map){
+        if (value === 'Admin'){
+            admins.push(key);
+        }
+    }
+
+    return admins;
+}
+
+const usuarios = new Map();
+
+usuarios.set('Luiz', 'Admin');
+usuarios.set('Steph', 'Admin');
+usuarios.set('Jorge', 'User');
+usuarios.set('Carlos', 'User');
+usuarios.set('Jesse', 'Admin');
+
+console.log(getAdmins(usuarios));
