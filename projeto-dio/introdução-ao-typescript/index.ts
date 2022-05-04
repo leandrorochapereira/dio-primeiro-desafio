1fed2630a337faae5fec553db0c5efce9972a646
Leo <lrp.0689@gmail.com>
2022-05-04 12:33:50 -0300

    Introdução ao TypeScrip

---
+++ projeto-dio/introdução-ao-typescript/index.ts
@@ -0,0 +1,26 @@
+//types
+//interfaces
+
+interface IAnimal {
+    nome: string;
+    tipo: 'terrestre' | 'aquático';
+    domestico: boolean;
+}
+
+interface IFelino extends IAnimal {
+    visaoNoturna: boolean;
+}
+
+interface ICanino extends IAnimal {
+    porte: 'pequeno' | 'medio' | 'grande';
+}
+
+type IDomestico = IFelino | ICanino;
+
+const animal: IDomestico = {
+    domestico: true,
+    nome: 'cachorro',
+    porte: 'medio',
+    tipo: 'terrestre',
+    visaoNoturna: true,
+}
